daac506a181c13096fe700c09e5202dc4de66f8c
Stu Anderson <stu.anderson@codeandtheory.com>
2024-07-25 15:59:44 -0500

    cool

--- components/agility-pages/index.ts
+++ components/agility-pages/index.ts
@@ -1,14 +1,14 @@
-import MainTemplate from "./MainTemplate";
+import MainPage from "./Main.page-model";
 
 // All of the Agility Page Template Components that are in use in this site need to be imported into this index file.
 // Place Page Templates in allTemplates array below, passing in a name and the component.
 
-const allTemplates = [{ name: "MainTemplate", template: MainTemplate }];
+const allTemplates = [{ name: "MainTemplate", template: MainPage }];
 
 export const getPageTemplate = (templateName: string) => {
   if (!templateName) return null;
   const obj = allTemplates.find(
-    (m) => m.name.toLowerCase() === templateName.toLowerCase(),
+    (m) => m.name.toLowerCase() === templateName.toLowerCase()
   );
   if (!obj) return null;
   return obj?.template;
